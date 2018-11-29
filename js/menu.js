cacd453f6c6fafffb3591c0552ede63bb4488040
datfit09 <phungdat.nd@gmail.com>
2018-11-29 16:02:54 +0700

    second commit

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// An/hien menu tren mobile.
 function toggleMenu() {
     var btn = $( '.toggle-menu-btn' ),
         overlay = $( '.menu-overlay' );
@@ -19,11 +20,92 @@ function toggleMenu() {
     } );
 }
 
+// Co dinh menu khi cuon trang.
+function navFixed() {
+    var nav       = jQuery( '.topnav' ),
+        navOffset = nav.offset().top;
+
+    jQuery( window ).on( 'scroll', function() {
+        var y = jQuery( this ).scrollTop();
+
+        if ( y >= navOffset ) {
+            nav.addClass( 'active' );
+        } else {
+            nav.removeClass( 'active' );
+        }
+    } );
+}
+
+// Di chuyen den section voi link tuong ung.
+function navScroll() {
+    jQuery( '.menu' ).on( 'click', 'a', function( e ) {
+        e.preventDefault();
+
+        // Dat bien.
+        var t      = jQuery( this ), // Gan chinh no = gia tri `t`,
+            target = t.attr( 'href' ); // Lay gia tri trong thuoc tinh `href`.
+
+        // Neu la `#` thi return, khong lam gi ca.
+        if ( '#' === target ) {
+            return;
+        }
+        
+        // Neu la nut Home thi cuon len tren dau.
+        if ( '#home' === target ) {
+            target = document.body;
+        }
+
+        // Thay trang thai active cua nut menu. Click vao nut nao nut ay la active.
+        t.parent().addClass( 'active' ).siblings().removeClass( 'active' );
+
+        // Cuon den section co ID tuong ung.
+        jQuery( document.documentElement, document.body ).stop().animate( {
+            scrollTop: jQuery( target ).offset().top
+        }, 500 );
+
+        return false;
+    } );
+}
+
+// Isotope gallery.
+function gallery() {
+    var iso = $( '.portfolio-content' ).isotope({
+        itemSelector: '.por-item',
+        layoutMode: 'fitRows'
+    });
+
+    var btn = jQuery( '.portfolio-button' );
+    btn.on( 'click', function( e ) {
+        e.preventDefault();
+
+        var t      = jQuery( this ),
+            data   = t.attr( 'data-filter' ),
+            output = '';
+
+        t.addClass( 'active' ).siblings().removeClass( 'active' );
+
+        if ( '*' !== data ) {
+            output = '.' + data;
+        }
+
+        iso.isotope({
+            filter: output
+        });
+    } );
+}
+
 (function($) {
     $( document ).ready( function() {
+        jQuery( window ).on( 'load', function() {
+            navFixed();
+            gallery();
+        } );
+
         toggleMenu();
+        navScroll();
     } );
 
+    // Menu tren mobile.
     $(document.body).on('click', '.sidebar-menu ul a', function(e) {
         e.preventDefault();
         var t = $(this);
